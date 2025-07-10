d22e761f91917acb506b0fe44b5e9df145cc11e5
Денис А. Жилин <zilin.d@sm.local>
2025-07-10 16:59:38 +0300

    homework-2 1-start

--- golang_1/dz/1-converter/project/main.go
+++ golang_1/dz/1-converter/project/main.go
@@ -9,3 +9,19 @@ func main() {
 
 	fmt.Print(EURInRUB)
 }
+
+func getUserInput() (float64, string, string) {
+	var quantity float64
+	var currentCurrency string
+	var targetCurrency string
+
+	fmt.Print("Введите сумму: ")
+	fmt.Scan(&quantity)
+	fmt.Print("Какая у вас валюта?Введите: ")
+	fmt.Scan(&currentCurrency)
+	fmt.Print("Какую валюты вы хотите?Введите: ")
+	fmt.Scan(&targetCurrency)
+	return quantity, currentCurrency, targetCurrency
+}
+
+func calcCurrency(quantity float64, currentCurrency string, targetCurrency string) {}
