5bac2502f73831f6cac37b9006ef891ddb46fabe
Денис А. Жилин <zilin.d@sm.local>
2025-07-25 15:47:34 +0300

    homework 10.9 1.1

--- golang_1/dz/3-struct/project-struct/main.go
+++ golang_1/dz/3-struct/project-struct/main.go
@@ -1,29 +1,5 @@
 package main
 
-import "time"
-
-type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
-}
-
-func newBin(id string, private bool, name string) *Bin {
-	return &Bin{
-		id:        id,
-		private:   private,
-		createdAt: time.Now(),
-		name:      name,
-	}
-}
-
-type BinList []Bin
-
-func (l *BinList) add(bin *Bin) {
-	*l = append(*l, *bin)
-}
-
 func main() {
 
 }
