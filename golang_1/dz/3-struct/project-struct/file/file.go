c2fc8a28cd80e2f3add6b02441c17ea846ced144
Денис А. Жилин <zilin.d@sm.local>
2025-07-30 19:20:24 +0300

    homework 11.15

--- golang_1/dz/3-struct/project-struct/file/file.go
+++ golang_1/dz/3-struct/project-struct/file/file.go
@@ -1,1 +1,21 @@
 package file
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func Read(path string) ([]byte, error) {
+	file, err := os.ReadFile(path)
+
+	if err != nil {
+		return nil, errors.New("__не удалось прочесть файл__")
+	}
+	return file, nil
+}
+
+func isJsonFile(path string) bool {
+	return strings.EqualFold(filepath.Ext(path), ".json")
+}
