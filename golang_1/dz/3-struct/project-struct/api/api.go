6bb2b06385803659d7f00c78402aab66abe4017b
Денис А. Жилин <zilin.d@sm.local>
2025-10-13 17:49:28 +0300

    env hw 1.2

--- golang_1/dz/3-struct/project-struct/api/api.go
+++ golang_1/dz/3-struct/project-struct/api/api.go
@@ -1,1 +1,14 @@
 package api
+
+import "project/download-json/config"
+
+type Api struct {
+	config string
+}
+
+func NewApi() *Api {
+	conf := config.NewConfig()
+	return &Api{
+		config: conf.Key,
+	}
+}
